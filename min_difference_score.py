62f5e4d24639c730a44acbb709e40fb02378b2e1
Liwen Huang <liwenhuang6@gmail.com>
2020-04-21 18:47:53 -0700

    odule and driver code

--- min_difference_score.py
+++ min_difference_score.py
@@ -1,16 +1,42 @@
-file_name = 'soccer.dat'
+import argparse
+import sys
 
-with open(file_name, 'r') as file:
-    for _ in range(3):
-        next(file)
-    min_difference = 1000000
-    for line in file:
-        line = line.split()
-        if len(line) > 5:
-            team_name, for_score, against_score = line[1], line[6], line[8]
-            score_difference = abs(int(for_score) - int(against_score))
-            if score_difference < min_difference:
-                min_difference = score_difference
-                min_team = team_name
+"""
+Build a Data Process class, contains a global minimum -- initiate to be a very big number -- keeping track of the minimum that has been seen
+function  caculate_min_score_difference processes the dat file line by line to calucate the absolution difference between A and F for each team, compare it with the global minimum and make modification accordingly
+function return the global minimum
+"""
 
-print("The team with the smallest difference in ‘for’ and ‘against’ goals is:", min_team, ", with score difference of", min_difference)
+class DataProcess:
+    min_difference = sys.maxsize
+
+    def caculate_min_score_difference(self, file_path):
+        with open(file_path, 'r') as file:
+            #skip the first few lines which are headers and line changes
+            for _ in range(3):
+                next(file)
+
+            for line in file:
+                line = line.split()
+                # filter the lines that do not contain the correct data format
+                if len(line) > 5:
+                    team_name, for_score, against_score = line[1], line[6], line[8]
+                    score_difference = abs(int(for_score) - int(against_score))
+                    if score_difference < self.min_difference:
+                        self.min_difference = score_difference
+                        min_team = team_name
+
+        print("The team with the smallest difference in ‘for’ and ‘against’ goals is:", min_team, ", with score difference of", self.min_difference)
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='a small program to process the wheather data')
+        parser.add_argument('-i', '--infile', help='the file name to be processed')
+        args = parser.parse_args()
+
+        ans = DataProcess()
+        if args.infile:
+            file_path = args.infile
+            ans.caculate_min_spread(file_path)
+        else:
+            file_path = 'soccer.dat'
+        ans.caculate_min_score_difference(file_path)
--- min_temp_spread.py
+++ min_temp_spread.py
@@ -1,16 +1,40 @@
-file_name = 'w_data (5).dat'
+import argparse
+import sys
 
-with open(file_name, 'r') as file:
-        for _ in range(6):
-            next(file)
-        min_spread = 100000000
-        for line in file:
-            line = line.split()
-            if len(line) > 3 and line[0] != 'mo':
-                day_number, max_temp, min_temp = line[0], line[1].strip("*"), line[2].strip("*")
-                spread = int(max_temp) - int(min_temp)
-                if spread < min_spread:
-                    min_spread = spread
-                    min_spread_day = day_number
+"""
+Build a Data Process class, contains a global minimum -- initiate to be a very big number -- keeping track of the minimum that has been seen
+function  caculate_min_spread process the dat file line by line to calucate the temperature spread for each month, compare it with the global minimum and make modification accordingly
+"""
 
-print("The smallest temperature spread in this month's date on day:", min_spread_day, "which is:", min_spread)
+class DataProcess:
+    min_spread = sys.maxsize
+
+    def caculate_min_spread(self, file_path):
+        with open(file_path, 'r') as file:
+            # skipping the first few lines that are headers, line changes
+            for _ in range(6):
+                next(file)
+
+            for line in file:
+                line = line.split()
+                if len(line) > 3 and line[0] != 'mo':
+                    day_number, max_temp, min_temp = line[0], line[1].strip("*"), line[2].strip("*")
+                    spread = int(max_temp) - int(min_temp)
+                    if spread < self.min_spread:
+                        self.min_spread = spread
+                        min_spread_day = day_number
+
+        print("The smallest temperature spread in this month's date on day:", min_spread_day, "which is:", self.min_spread)
+
+if __name__ == '__main__':
+        parser = argparse.ArgumentParser(description='a small program to process the wheather data')
+        parser.add_argument('-i', '--infile', help='the file name to be processed')
+        args = parser.parse_args()
+
+        ans = DataProcess()
+        if args.infile:
+            file_path = args.infile
+            ans.caculate_min_spread(file_path)
+        else:
+            file_path = 'w_data (5).dat'
+        ans.caculate_min_spread(file_path)
